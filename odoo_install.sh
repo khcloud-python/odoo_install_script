53b2de42ae553ed43d143e632a9ca57b39f654fa
wdmsyf <wdmsyf@sina.com>
2019-10-23 19:24:42 +0800

    Add python module: num2words XlsxWriter xlrd

--- odoo_install.sh
+++ odoo_install.sh
@@ -109,6 +109,9 @@ sudo apt-get install git python3 python3-pip build-essential wget python3-dev py
 echo -e "\n---- Install python packages/requirements ----"
 sudo pip3 install -r https://github.com/odoo/odoo/raw/${OE_VERSION}/requirements.txt
 
+echo -e "\n---- Install python libraries depend by others ----"
+sudo pip3 install num2words ofxparse xmltodict XlsxWriter xlwt xlrd pycryptodome
+
 echo -e "\n---- Installing nodeJS NPM and rtlcss for LTR support ----"
 sudo apt-get install nodejs npm
 sudo npm install -g rtlcss
@@ -167,7 +170,7 @@ if [ $IS_ENTERPRISE = "True" ]; then
 
     echo -e "\n---- Added Enterprise code under $OE_HOME/enterprise/addons ----"
     echo -e "\n---- Installing Enterprise specific libraries ----"
-    sudo pip3 install num2words ofxparse xmltodict xlwt pycryptodome
+    #sudo pip3 install num2words ofxparse xmltodict xlwt pycryptodome
     sudo npm install -g less
     sudo npm install -g less-plugin-clean-css
 fi
